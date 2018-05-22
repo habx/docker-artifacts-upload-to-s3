1494a6be5ffb3fcee22fafe568da9f8b8827ac62
ael <ael@remote>
2018-05-22 11:11:38 +0000

    print url at the end

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ RUN apt-get update && apt-get install -y python python-pip curl unzip groff
 RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 RUN pip install awscli
 
-COPY entrypoint.sh .
+COPY entrypoint.sh /bin/
 
-CMD ["./entrypoint.sh"]
+CMD ["/bin/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 DST="s3://${PLUGIN_S3_REPO}/${DRONE_REPO_NAME}/$(date -d @${DRONE_BUILD_CREATED} --rfc-3339=date)/$(date -d @${DRONE_BUILD_CREATED} +%H-%M)/"
+URL="https://${PLUGIN_S3_REPO}.s3.amazonaws.com/${DRONE_REPO_NAME}/$(date -d @${DRONE_BUILD_CREATED} --rfc-3339=date)/$(date -d @${DRONE_BUILD_CREATED} +%H-%M)/${ARTIFACT}"
 CMD="aws s3 cp ${ARTIFACT} ${DST}"
 echo $CMD
 $CMD
+
+echo $URL
